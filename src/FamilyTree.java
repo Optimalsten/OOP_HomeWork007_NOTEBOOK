e90afc11deef0a37d2e627c5b0e224280000c97d
Optimalsten <soa@prg.spb.ru>
2024-10-22 23:45:05 +0300

    first variant Homework 01 OOP 22102024

---
+++ src/FamilyTree.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Класс для работы с генеалогическим древом
+
+class FamilyTree {
+    private List<Person> people;
+
+    public FamilyTree() {
+        this.people = new ArrayList<>();
+    }
+
+    public void addPerson(Person person) {
+        this.people.add(person);
+    }
+
+    public List<Person> getChildren(Person parent) {
+        return parent.getChildren();
+    }
+
+    public Person findPersonByName(String name) {
+        for (Person person : people) {
+            if (person.getName().equals(name)) {
+                return person;
+            }
+        }
+        return null;
+    }
+}
